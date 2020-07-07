84f967a66a39db47a2cdc1b129ef3337e7830415
Skogkatt <wayne79687968@gmail.com>
2020-07-07 17:05:11 +0800

    add login page

--- resources/views/components/admin/admin-master.blade.php
+++ resources/views/components/admin/admin-master.blade.php
@@ -32,7 +32,15 @@
                         <a class="dropdown-item" href="#">Settings</a>
                         <a class="dropdown-item" href="#">Activity Log</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="login.html">Logout</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
                 </li>
             </ul>
